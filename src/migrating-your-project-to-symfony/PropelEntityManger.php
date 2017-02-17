a2a205ace4a45697f28e347980345e00c190f7d4
Yannick de Lange <yannick.l.88@gmail.com>
2017-02-17 08:43:17 +0100

    Update PropelEntityManger.php

--- src/migrating-your-project-to-symfony/PropelEntityManger.php
+++ src/migrating-your-project-to-symfony/PropelEntityManger.php
@@ -1,297 +1,420 @@
 <?php
 namespace Propel\Orm;
 
-use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\DBAL\DBALException;
-use Doctrine\ORM\ORMException;
+use Doctrine\Common\Persistence\Mapping\MappingException;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\ORMInvalidArgumentException;
+use Doctrine\ORM\Query\ResultSetMapping;
+use Symfony\Component\Intl\Exception\NotImplementedException;
 
 /**
- * Provides the same API as Doctrine's Entity Manager to keep things consistent between "entity managers".
+ * PropelEntityManager which allows for a doctrine way of interacting with
+ * propel objects.
  */
-class PropelEntityManager implements ObjectManager
+class PropelEntityManager implements EntityManagerInterface
 {
-    /**
-     * @var string
-     */
-    private $name;
+    private $db_host;
+    private $db_name;
+    private $db_port;
+    private $db_user;
+    private $db_password;
+    private $db_encoding;
+    private $debug;
+
+    private $inserts = [];
+    private $deletes = [];
+    private $changes = [];
 
     /**
-     * @var array
+     * @param string $db_host
+     * @param string $db_name
+     * @param string $db_port
+     * @param string $db_user
+     * @param string $db_password
+     * @param string $db_encoding
      */
-    private $save = [];
+    public function __construct(
+        $db_host,
+        $db_name,
+        $db_port,
+        $db_user,
+        $db_password,
+        $db_encoding,
+        $debug = false
+    ) {
+        $this->db_host     = $db_host;
+        $this->db_name     = $db_name;
+        $this->db_port     = $db_port;
+        $this->db_user     = $db_user;
+        $this->db_password = $db_password;
+        $this->db_encoding = $db_encoding;
+        $this->debug       = $debug;
 
-    /**
-     * @var array
-     */
-    private $delete = [];
-
-    /**
-     * @param string $name
-     */
-    public function __construct($name)
-    {
-        $this->name = $name;
+        // init the connection
+        \Propel::setConfiguration($this->getConfiguration());
     }
 
     /**
-     * @return \Connection
+     * @see \Doctrine\ORM\EntityManagerInterface::getConnection()
      */
     public function getConnection()
     {
-        return \Propel::getConnection($this->name);
+        return \Propel::getConnection();
     }
 
     /**
-     * @return \DatabaseMap
+     * @see \Doctrine\ORM\EntityManagerInterface::getExpressionBuilder()
      */
-    public function getDatabaseMap()
+    public function getExpressionBuilder()
     {
-        return \Propel::getDatabaseMap($this->name);
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
     }
 
     /**
-     * {@inheritdoc}
+     * @see \Doctrine\ORM\EntityManagerInterface::beginTransaction()
+     */
+    public function beginTransaction()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::transactional()
+     */
+    public function transactional($func)
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::commit()
+     */
+    public function commit()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::rollback()
+     */
+    public function rollback()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::createQuery()
+     */
+    public function createQuery($dql = '')
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::createNamedQuery()
+     */
+    public function createNamedQuery($name)
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::createNativeQuery()
+     */
+    public function createNativeQuery($sql, ResultSetMapping $rsm)
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::createNamedNativeQuery()
+     */
+    public function createNamedNativeQuery($name)
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::createQueryBuilder()
+     */
+    public function createQueryBuilder()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::getReference()
+     */
+    public function getReference($entityName, $id)
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::getPartialReference()
+     */
+    public function getPartialReference($entityName, $identifier)
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::close()
+     */
+    public function close()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::copy()
+     */
+    public function copy($entity, $deep = false)
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::lock()
+     */
+    public function lock($entity, $lockMode, $lockVersion = null)
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::getEventManager()
+     */
+    public function getEventManager()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::getConfiguration()
+     */
+    public function getConfiguration()
+    {
+        return [
+            'datasources' => [
+                "default" => "myhostnet",
+                'myhostnet' => [
+                    'adapter' => 'mysql',
+                    'connection' => [
+                        'dsn' => 'mysql:host='. $this->db_host .';dbname='.$this->db_name.';port='.$this->db_port,
+                        'user' => $this->db_user,
+                        'password' => $this->db_password,
+                        'classname' => $this->debug ? 'HostnetDebugPDO' : 'PropelPDO',
+                        'settings' => [
+                            'charset' => [
+                                'value' => $this->db_encoding
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::isOpen()
+     */
+    public function isOpen()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::getUnitOfWork()
+     */
+    public function getUnitOfWork()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::getHydrator()
+     */
+    public function getHydrator($hydrationMode)
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::newHydrator()
+     */
+    public function newHydrator($hydrationMode)
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::getProxyFactory()
+     */
+    public function getProxyFactory()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::getFilters()
+     */
+    public function getFilters()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::isFiltersStateClean()
+     */
+    public function isFiltersStateClean()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\ORM\EntityManagerInterface::hasFilters()
+     */
+    public function hasFilters()
+    {
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
+    }
+
+    /**
+     * @see \Doctrine\Common\Persistence\ObjectManager::find()
      */
     public function find($class_name, $id)
     {
-        $peer_class = $this->getPeerClass($class_name);
-        $table_map  = $this->getDatabaseMap()->getTable($peer_class::TABLE_NAME);
+        $peer_class = $class_name."Peer";
 
-        $args = func_get_args();
-        array_shift($args);
-
-        $index = 0;
-        $c     = new \Criteria();
-
-        foreach ($this->getPrimaryKeys($table_map) as $column) {
-            if (isset($args[$index])) {
-                $c->add($column, $args[$index]);
-            }
-            $index++;
+        if (!class_exists($peer_class)) {
+            throw MappingException::nonExistingClass($peer_class);
         }
 
-        return $peer_class::doSelectOne($c, $this->getConnection());
+        $o = $peer_class::retrieveByPK($id);
+
+        if ($o !== null) {
+            $this->changes[] = $o;
+        }
+
+        return $o;
     }
 
     /**
-     * {@inheritdoc}
+     * @see \Doctrine\Common\Persistence\ObjectManager::persist()
      */
     public function persist($object)
     {
-        if (!$object instanceof \Persistent) {
-            throw new \InvalidArgumentException('Object must be instance of Persistent.');
+        if (!($object instanceof \Persistent)) {
+            throw ORMInvalidArgumentException::invalidObject('EntityManager#persist()', $object);
         }
-        $this->save[] = $object;
+
+        $this->inserts[] = $object;
     }
 
     /**
-     * {@inheritdoc}
+     * @see \Doctrine\Common\Persistence\ObjectManager::remove()
      */
     public function remove($object)
     {
-        if (!$object instanceof \Persistent) {
-            throw new \InvalidArgumentException('Object must be instance of Persistent.');
+        if (!($object instanceof \Persistent)) {
+            throw ORMInvalidArgumentException::invalidObject('EntityManager#persist()', $object);
         }
-        $this->delete[] = $object;
+
+        $this->deletes[] = $object;
     }
 
     /**
-     * {@inheritdoc}
+     * @see \Doctrine\Common\Persistence\ObjectManager::merge()
      */
     public function merge($object)
     {
-        throw new \BadMethodCallException(__METHOD__ . ' is not supported on Propel objects.');
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
     }
 
     /**
-     * {@inheritdoc}
+     * @see \Doctrine\Common\Persistence\ObjectManager::clear()
      */
     public function clear($object_name = null)
     {
-        if ($object_name) {
-            throw new \BadMethodCallException('Object-specific Clear is not supported on Propel objects.');
-        }
-        $this->delete = [];
-        $this->save   = [];
+        $this->changes = [];
+        $this->deletes = [];
+        $this->inserts = [];
     }
 
     /**
-     * {@inheritdoc}
+     * @see \Doctrine\Common\Persistence\ObjectManager::detach()
      */
     public function detach($object)
     {
-        throw new \BadMethodCallException(__METHOD__ . ' is not supported on Propel objects.');
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
     }
 
     /**
-     * {@inheritdoc}
+     * @see \Doctrine\Common\Persistence\ObjectManager::refresh()
      */
     public function refresh($object)
     {
-        if (!$object instanceof \Persistent) {
-            throw new \InvalidArgumentException('Object must be instance of Persistent.');
-        }
-
-        // Remove this object from the save/delete collection.
-        foreach ($this->save as $k => $v) {
-            if ($v === $object) {
-                unset($this->save[$k]);
-                break;
-            }
-        }
-
-        foreach ($this->delete as $k => $v) {
-            if ($v === $object) {
-                unset($this->delete[$k]);
-                break;
-            }
-        }
-
-        $pk_list = $object->getPrimaryKey();
-
-        if (!is_array($pk_list)) {
-            $pk_list = [$pk_list];
-        }
-
-        $peer = get_class($object->getPeer());
-        return call_user_func_array([$this, 'find'], array_merge([$peer], $pk_list));
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
     }
 
     /**
-     * {@inheritdoc}
-     *
-     * Additionally supports flushing 1 object or an array of objects.
+     * @see \Doctrine\Common\Persistence\ObjectManager::flush()
      */
     public function flush()
     {
-        $args    = func_get_args();
-        $to_save = [];
-
-        if (isset($args[0])) {
-            $to_save = $args[0] instanceof \Persistent
-                ? [$args[0]]
-                : $args[0];
+        foreach ($this->deletes as $delete) {
+            $delete->delete();
         }
 
-        if (count($to_save) > 0) {
-            $this->doSave($to_save);
-            return;
+        $changes = array_merge($this->inserts, array_filter($this->changes, function (\BaseObject $o) {
+            return $o->isModified() && !$o->isDeleted();
+        }));
+
+        foreach ($changes as $change) {
+            $change->save();
         }
 
-        foreach ($this->delete as $object) {
-            if (!$object->isNew()) {
-                $object->delete();
-            }
-        }
-
-        $this->doSave($this->save);
-
-        $this->delete = [];
-        $this->save   = [];
+        $this->clear();
     }
 
     /**
-     * @param \Persistent[] $objects
-     * @throws \Exception
+     * @see \Doctrine\Common\Persistence\ObjectManager::getRepository()
      */
-    private function doSave(array $objects)
+    public function getRepository($className)
     {
-        foreach ($objects as $object) {
-            if (!$object->isDeleted()) {
-                $object->save();
-            }
-        }
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
     }
 
     /**
-     * {@inheritdoc}
+     * @see \Doctrine\Common\Persistence\ObjectManager::getClassMetadata()
      */
-    public function getRepository($class_name)
+    public function getClassMetadata($className)
     {
-        return new PropelRepository($this, $this->getPeerClass($class_name));
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function getClassMetadata($class_name)
-    {
-        throw new \BadMethodCallException(__METHOD__ . ' is not supported on Propel objects.');
-    }
-
-    /**
-     * {@inheritdoc}
+     * @see \Doctrine\Common\Persistence\ObjectManager::getMetadataFactory()
      */
     public function getMetadataFactory()
     {
-        throw new \BadMethodCallException(__METHOD__ . ' is not supported on Propel objects.');
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
     }
 
     /**
-     * {@inheritdoc}
+     * @see \Doctrine\Common\Persistence\ObjectManager::initializeObject()
      */
     public function initializeObject($obj)
     {
-        throw new \BadMethodCallException(__METHOD__ . ' is not supported on Propel objects.');
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
     }
 
     /**
-     * {@inheritdoc}
+     * @see \Doctrine\Common\Persistence\ObjectManager::contains()
      */
     public function contains($object)
     {
-        return in_array($object, $this->save) || in_array($object, $this->delete);
-    }
-
-    /**
-     * Returns the name of the Peer-class of the given class name.
-     *
-     * @param  string $class_name
-     * @throws ORMException
-     * @throws DBALException
-     * @return string
-     */
-    private function getPeerClass($class_name)
-    {
-        if (substr($class_name, 0, 1) !== '\\') {
-            $class_name = '\\' . $class_name;
-        }
-
-        if (substr($class_name, strlen($class_name) - 4) !== 'Peer') {
-            $class_name .= 'Peer';
-        }
-
-        if (!class_exists($class_name)) {
-            throw new ORMException('Repository "' . $class_name . '" does not exist.');
-        }
-
-        if (!defined($class_name . '::DATABASE_NAME')) {
-            throw new DBALException('Class "' . $class_name . '" is not a valid repository.');
-        }
-
-        if ($class_name::DATABASE_NAME !== $this->getDatabaseMap()->getName()) {
-            throw new ORMException(
-                'Repository "'
-                . $class_name
-                . '" does not belong to the "'
-                . $this->getDatabaseMap()->getName()
-                . '" manager.'
-            );
-        }
-        return $class_name;
-    }
-
-    /**
-     * @param \TableMap $table_map
-     */
-    private function getPrimaryKeys(\TableMap $table_map)
-    {
-        $result = [];
-        foreach ($table_map->getColumns() as $column) {
-            if ($column->isPrimaryKey()) {
-                $result[] = $table_map->getName() . '.' . $column->getColumnName();
-            }
-        }
-        return $result;
+        throw new NotImplementedException("Method " . __FUNCTION__ . " is not implemented.");
     }
 }
